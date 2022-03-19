64e24732a4ee85f854f2db5a8a676bbd2c019785
GreedNeSS <ruslan199126@gmail.com>
2022-03-19 15:10:59 +0300

    Добавьте файлы проекта.

---
+++ DatabaseConnection/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+string connectionString = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True";
+
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    await connection.OpenAsync();
+    Console.WriteLine("Подключение открыто!");
+    Console.WriteLine("\nСвойства подключения:");
+    Console.WriteLine($"\tСтрока подключения: {connection.ConnectionString}");
+    Console.WriteLine($"\tБаза данных: {connection.Database}");
+    Console.WriteLine($"\tСервер: {connection.DataSource}");
+    Console.WriteLine($"\tВерсия сервера: {connection.ServerVersion}");
+    Console.WriteLine($"\tСостояние: {connection.State}");
+    Console.WriteLine($"\tWorkstation Id: {connection.WorkstationId}");
+}
+
+Console.WriteLine("Подключение закрыто");
+Console.WriteLine("Программа завершила работу.");
+Console.Read();
