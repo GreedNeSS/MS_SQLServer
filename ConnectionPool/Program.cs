0a9267d3da5f8b93a6c5f5ba8dc7e59e85093beb
GreedNeSS <ruslan199126@gmail.com>
2022-03-19 15:33:34 +0300

    Added connection pool example.

---
+++ ConnectionPool/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+
+Console.WriteLine("***** Connection Pool *****");
+
+string connectionString1 = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True;";
+string connectionString2 = "Server=(localdb)\\mssqllocaldb;Database=tempdb;Trusted_Connection=True;";
+
+using (SqlConnection sqlConnection = new(connectionString1))
+{
+    await sqlConnection.OpenAsync();
+    Console.WriteLine($"Id: {sqlConnection.ClientConnectionId}");
+}
+
+using (SqlConnection sqlConnection = new(connectionString2))
+{
+    await sqlConnection.OpenAsync();
+    Console.WriteLine($"Id: {sqlConnection.ClientConnectionId}");
+}
+
+//SqlConnection.ClearAllPools();
+
+using (SqlConnection sqlConnection = new(connectionString1))
+{
+    await sqlConnection.OpenAsync();
+    Console.WriteLine($"Id: {sqlConnection.ClientConnectionId}");
+}
+
+Console.WriteLine("Программа завершила работу!");
+Console.Read();
