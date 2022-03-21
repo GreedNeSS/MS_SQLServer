7f825438b37d295eef07dcde378abbc28315ab15
GreedNeSS <ruslan199126@gmail.com>
2022-03-21 18:28:15 +0300

    Using ExecuteScalarAsync

---
+++ GettingScalarValues/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+
+Console.WriteLine("***** Getting Scalar Values *****");
+
+string connectionString = "Server=localhost;Database=adonetdb;Trusted_Connection=True;Encrypt=False";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    await connection.OpenAsync();
+
+    SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Users", connection);
+    object userCount = await command.ExecuteScalarAsync();
+
+    command.CommandText = "SELECT MIN(Age) FROM Users";
+    object minAge = await command.ExecuteScalarAsync();
+
+    command.CommandText = "SELECT AVG(Age) FROM Users";
+    object avgAge = await command.ExecuteScalarAsync();
+
+    Console.WriteLine($"В таблице {userCount} обьектов!");
+    Console.WriteLine($"Минимальный возраст {minAge}");
+    Console.WriteLine($"Средний возраст {avgAge}");
+}
+
+Console.ReadLine();
