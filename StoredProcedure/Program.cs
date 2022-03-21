d650a13a2f0ac43093fb8de0de6161c8081b0cbd
GreedNeSS <ruslan199126@gmail.com>
2022-03-22 00:08:59 +0300

    Added StoredProcedure example.

---
+++ StoredProcedure/Program.cs
@@ -0,0 +1,37 @@
+﻿using StoredProcedure;
+
+Console.WriteLine("***** Stored procedure *****");
+
+string conStr = "Server=localhost;Database=adonetdb;Trusted_Connection=True;Encrypt=False";
+
+Console.WriteLine("Введите имя пользователя: ");
+string name = Console.ReadLine();
+Console.WriteLine("Введите возраст пользователя: ");
+int age = int.Parse(Console.ReadLine());
+
+Dictionary<string, string> parameters = new Dictionary<string, string>();
+parameters["@name"] = name;
+parameters["@age"] = age.ToString();
+
+string proc1 = @"CREATE PROCEDURE [dbo].[sp_InsertUser] 
+@name nvarchar(50), @age int 
+AS 
+INSERT INTO Users (Name, Age) VALUES (@name, @age)
+SELECT SCOPE_IDENTITY()
+GO";
+string proc2 = @"CREATE PROCEDURE [dbo].[sp_GetUsers]
+AS
+SELECT * FROM Users
+GO";
+
+StoredProcedureClass procedureClass = new StoredProcedureClass();
+
+await procedureClass.CreateProcedureAsync(conStr, proc1);
+await procedureClass.CreateProcedureAsync(conStr, proc2);
+
+Console.WriteLine();
+
+await procedureClass.ExecutingProcedureAsync(conStr, "sp_InsertUser",
+    StoredProcedureClass.ExecuteType.ExecuteScalarAsync, parameters);
+
+await procedureClass.ExecutingProcedureAsync(conStr, "sp_GetUsers", StoredProcedureClass.ExecuteType.ExecuteReaderAsync);
---
+++ StoredProcedure/StoredProcedureClass.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace StoredProcedure
+{
+    internal class StoredProcedureClass
+    {
+        public enum ExecuteType
+        {
+            ExecuteReaderAsync,
+            ExecuteNonQueryAsync,
+            ExecuteScalarAsync,
+        }
+
+        public async Task CreateProcedureAsync(string connectionString, string sqlExpression)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                try
+                {
+                    await connection.OpenAsync();
+                    await command.ExecuteNonQueryAsync();
+
+                    Console.WriteLine("Процедура добавлена в базу данных!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public async Task ExecutingProcedureAsync(string connectionString,
+            string procedureName, ExecuteType executeType, Dictionary<string, string> parameters = null)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(procedureName, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                if (parameters is not null)
+                {
+                    foreach (var (key, value) in parameters)
+                    {
+                        command.Parameters.AddWithValue(key, value);
+                    }
+                }
+
+                try
+                {
+                    await connection.OpenAsync();
+                    switch (executeType)
+                    {
+                        case ExecuteType.ExecuteReaderAsync:
+                            using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                            {
+                                if (reader.HasRows)
+                                {
+                                    int count = reader.FieldCount;
+
+                                    for (int i = 0; i < count; i++)
+                                        Console.Write(reader.GetName(i) + "\t");
+
+                                    Console.WriteLine();
+
+                                    while (await reader.ReadAsync())
+                                    {
+                                        for (int i = 0; i < count; i++)
+                                            Console.Write(reader.GetValue(i) + "\t");
+
+                                        Console.WriteLine();
+                                    }
+                                }
+                            }
+                            break;
+
+                        case ExecuteType.ExecuteNonQueryAsync:
+                            await command.ExecuteNonQueryAsync();
+                            Console.WriteLine("Запрос выполнен!");
+                            break;
+
+                        case ExecuteType.ExecuteScalarAsync:
+                            object num = await command.ExecuteScalarAsync();
+                            Console.WriteLine($"Result: {num}");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
