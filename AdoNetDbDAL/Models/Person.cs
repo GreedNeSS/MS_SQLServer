0459ff5c90781830ff9dc8493f8c49d2cf22e9cc
GreedNeSS <ruslan199126@gmail.com>
2022-03-25 18:17:26 +0300

    Added ctors.

--- AdoNetDbDAL/Models/Person.cs
+++ AdoNetDbDAL/Models/Person.cs
@@ -12,6 +12,18 @@ namespace AdoNetDbDAL.Models
         public string Name { get; set; }
         public int Age { get; set; }
 
+        public Person()
+        {
+
+        }
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
         public override string ToString()
         {
             return $"ID: {Id}, Name: {Name}, Age: {Age}";
